c5245b6b19d9c0d22f03ecd130d03053774accd0
Aleja <u20211195668@usco.edu.co>
2025-07-28 11:20:57 -0500

    refactor: remove BrowserRouter wrapper from main.jsx and delete 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // 👈 Agregar esto
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* 👈 Envolver aquí */}
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
